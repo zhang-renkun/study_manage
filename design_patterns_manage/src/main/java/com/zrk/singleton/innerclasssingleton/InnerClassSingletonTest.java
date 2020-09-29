d2bd3ff1554565f86654f4c172c0eb5da16319fa
zhangrenkun <zrk18801040353@163.com>
2020-09-30 00:57:51 +0800

    chushihua

---
+++ design_patterns_manage/src/main/java/com/zrk/singleton/innerclasssingleton/InnerClassSingletonTest.java
@@ -0,0 +1,90 @@
+package com.zrk.singleton.innerclasssingleton;
+
+import java.io.*;
+
+/**
+ * This is Description
+ *
+ * @author zhangrenkun
+ * @date 2020/09/29
+ * @Slogan 以后的你一定会感谢现在努力的自己！
+ */
+public class InnerClassSingletonTest {
+
+//    public static void main(String[] args) {
+//        new Thread(() -> {
+//            InnerClassSingleton instance = InnerClassSingleton.getInstance();
+//            System.out.println(instance);
+//        }).start();
+//        new Thread(() -> {
+//            InnerClassSingleton instance = InnerClassSingleton.getInstance();
+//            System.out.println(instance);
+//        }).start();
+//    }
+
+
+    /**
+     * 反射攻击
+     *
+     * @param args
+     */
+//    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+//        // 获取实例
+//        Constructor<InnerClassSingleton> declaredConstructor = InnerClassSingleton.class.getDeclaredConstructor();
+//        // 开放权限
+//        declaredConstructor.setAccessible(true);
+//        InnerClassSingleton innerClassSingleton = declaredConstructor.newInstance();
+//
+//        InnerClassSingleton instance = InnerClassSingleton.getInstance();
+//        System.out.println(innerClassSingleton == instance);
+//
+//    }
+
+    /**
+     * 序列化
+     */
+    public static void main(String[] args) throws Exception {
+        InnerClassSingleton instance = InnerClassSingleton.getInstance();
+
+        // 序列化
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("testSerializable"));
+        oos.writeObject(instance);
+        oos.close();
+
+        // 反序列化
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("testSerializable"));
+        InnerClassSingleton o = (InnerClassSingleton) ois.readObject();
+
+        System.out.println(instance == o);
+    }
+}
+
+class InnerClassSingleton implements Serializable {
+
+    static final long serialVersionUID = 43L;
+
+    /**
+     * 静态内部类
+     */
+    private static class InnerClassHolder {
+        private static InnerClassSingleton instance = new InnerClassSingleton();
+    }
+
+    public InnerClassSingleton() {
+        // TODO 防止反射攻击
+        if (InnerClassHolder.instance != null)
+            throw new RuntimeException("单例不允许多个实例");
+    }
+
+    public static InnerClassSingleton getInstance() {
+        // TODO 在调用这个方法的时候再去加载InnerClassHolder类，从而才获取实例
+        return InnerClassHolder.instance;
+    }
+
+    /**
+     * 指定一个替代方案来从流中获取实例
+     */
+    Object readResolve() throws ObjectStreamException{
+        return InnerClassHolder.instance;
+    }
+}
---
+++ design_patterns_manage/src/main/java/com/zrk/singleton/lazysingleton/LazySingletonTest.java
@@ -0,0 +1,95 @@
+package com.zrk.singleton.lazysingleton;
+
+/**
+ * This is Description
+ *
+ * @author zhangrenkun
+ * @date 2020/09/29
+ * @Slogan 以后的你一定会感谢现在努力的自己！
+ */
+public class LazySingletonTest {
+
+    public static void main(String[] args) {
+        /**
+         * 但线程访问下创建的两个实例对象时一样的
+         */
+//        test01();
+        /**
+         * 多线程访问下存在线程安全问题
+         */
+//        test02();
+        /**
+         * 最简单的解决方式：在构造方法上面加 synchronized
+         * 即使这样，在多线程的情况下也会出现多条线程实例化时加多个锁
+         */
+//        test03();
+        /**
+         * 双重check优化：在内部类上面加 synchronized
+         */
+        test04();
+    }
+
+    private static void test04() {
+        new Thread(() -> {
+            LazySingleton instance = LazySingleton.getInstance();
+            System.out.println(instance);
+        }).start();
+
+        new Thread(() -> {
+            LazySingleton instance = LazySingleton.getInstance();
+            System.out.println(instance);
+        }).start();
+    }
+
+    private static void test03() {
+        new Thread(() -> {
+            LazySingleton instance = LazySingleton.getInstance();
+            System.out.println(instance);
+        }).start();
+
+        new Thread(() -> {
+            LazySingleton instance = LazySingleton.getInstance();
+            System.out.println(instance);
+        }).start();
+    }
+
+    private static void test02() {
+        new Thread(() -> {
+            LazySingleton instance = LazySingleton.getInstance();
+            System.out.println(instance);
+        }).start();
+
+        new Thread(() -> {
+            LazySingleton instance = LazySingleton.getInstance();
+            System.out.println(instance);
+        }).start();
+    }
+
+    private static void test01() {
+        LazySingleton instance = LazySingleton.getInstance();
+        LazySingleton instance1 = LazySingleton.getInstance();
+        System.out.println(instance == instance1);
+    }
+
+
+}
+
+class LazySingleton {
+    /**
+     * volatile 防止重排序
+     */
+    private volatile static LazySingleton instance;
+
+    public LazySingleton() {
+    }
+
+    public static LazySingleton getInstance() {
+        if (instance == null) {
+            synchronized (LazySingleton.class) {
+                if (instance == null)
+                    instance = new LazySingleton();
+            }
+        }
+        return instance;
+    }
+}
