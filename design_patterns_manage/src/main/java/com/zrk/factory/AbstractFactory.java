4fea2b2fde3d776ea71535f15d0e0d11fdd6bb3e
zhangrenkun <zrk18801040353@163.com>
2020-10-09 21:03:48 +0800

    抽象工厂模式

---
+++ design_patterns_manage/src/main/java/com/zrk/factory/AbstractFactory.java
@@ -0,0 +1,70 @@
+package com.zrk.factory;
+
+/**
+ * This is Description
+ *
+ * @author zhangrenkun
+ * @date 2020/10/09
+ * @Slogan 以后的你一定会感谢现在努力的自己！
+ */
+public class AbstractFactory {
+
+    public static void main(String[] args) {
+        IBaseUtils baseUtils = new MysqlBaseUtils();
+        baseUtils.getConnect().connect();
+        baseUtils.getCommand().command();
+    }
+
+}
+
+/**
+ * 公共连接接口
+ */
+interface IConnection {
+    void connect();
+}
+
+class MysqlConnetion implements IConnection {
+
+    @Override
+    public void connect() {
+        System.out.println("Mysql Connetion");
+    }
+}
+
+/**
+ * 公共执行接口
+ */
+interface ICommand {
+    void command();
+}
+
+class MysqlCommand implements ICommand {
+
+    @Override
+    public void command() {
+        System.out.println("Mysql Command");
+    }
+}
+
+
+interface IBaseUtils {
+
+    IConnection getConnect();
+
+    ICommand getCommand();
+}
+
+class MysqlBaseUtils implements IBaseUtils {
+
+    @Override
+    public IConnection getConnect() {
+        return new MysqlConnetion();
+    }
+
+    @Override
+    public ICommand getCommand() {
+        return new MysqlCommand();
+    }
+}
+
